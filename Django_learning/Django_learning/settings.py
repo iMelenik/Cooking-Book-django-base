593b0b1c54c08fffa6d8e8071ba4adfc13810a89
runmycatrun <dteamaker@gmail.com>
2022-11-28 14:54:39 +0300

    complete remake from articles to recipes

--- Django_learning/Django_learning/settings.py
+++ Django_learning/Django_learning/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'articles.apps.ArticlesConfig',
     'recipes.apps.RecipesConfig',
 ]
 
